d3f87b26d4086a15225f41870e8be44f92b5d45e
lhtung <xhaitungx@gmail.com>
2022-04-21 20:46:52 +0700

    css detail and introduce in detailpage

--- src/container/detailTour/TourDetail.jsx
+++ src/container/detailTour/TourDetail.jsx
@@ -1,11 +1,33 @@
 import React, { useState } from "react";
 import ButtonCustom from "../../components/buttonCustom/ButtonCustom";
 
+import "./detailTour.scss";
 const TourDetail = ({ tourData }) => {
   const [introductionOn, setIntroduction] = useState(true);
 
-  const renderIntroduce = () => <></>;
-
+  const renderIntroduction = (diaDiem, index) => (
+    <div className={`introduction--item ${index % 2 === 0 ? "even" : "odd"}`}>
+      <img
+        src={`https://tour-api-dev.herokuapp.com/${diaDiem.id_dia_diem.hinh}`}
+      />
+      <div className="place--infor">
+        <h1>{diaDiem.id_dia_diem.ten}</h1>
+        <p>{diaDiem.id_dia_diem.mo_ta}</p>
+      </div>
+    </div>
+  );
+  const renderDetail = (diaDiem, index) => (
+    <div className={`detail--item ${index % 2 === 0 ? "even" : "odd"}`}>
+      <img
+        src={`https://tour-api-dev.herokuapp.com/${diaDiem.id_dia_diem.hinh}`}
+      />
+      <div className="place--infor">
+        <h1>{diaDiem.id_dia_diem.ten}</h1>
+        <p>{diaDiem.dich_vu}</p>
+        <p>{diaDiem.ngay_o} ngày</p>
+      </div>
+    </div>
+  );
   return (
     <>
       <div className="tour--information__detail">
@@ -40,26 +62,19 @@ const TourDetail = ({ tourData }) => {
         </div>
 
         <div className="content">
-          {introductionOn
-            ? tourData.lich_trinh?.map((diaDiem) => (
-                <>
-                  <img
-                    src={`https://tour-api-dev.herokuapp.com/${diaDiem.id_dia_diem.hinh}`}
-                  />
-                  <div>{diaDiem.id_dia_diem.ten}</div>
-                  <div>{diaDiem.id_dia_diem.mo_ta}</div>
-                </>
-              ))
-            : tourData.lich_trinh?.map((diaDiem) => (
-                <>
-                  <img
-                    src={`https://tour-api-dev.herokuapp.com/${diaDiem.id_dia_diem.hinh}`}
-                  />
-                  <div>{diaDiem.id_dia_diem.ten}</div>
-                  <div>{diaDiem.dich_vu}</div>
-                  <div>{diaDiem.ngay_o} ngày</div>
-                </>
-              ))}
+          {introductionOn ? (
+            <div className="introduction--container">
+              {tourData.lich_trinh?.map((diaDiem, index) =>
+                renderIntroduction(diaDiem, index)
+              )}
+            </div>
+          ) : (
+            <div className="detail--container">
+              {tourData.lich_trinh?.map((diaDiem, index) =>
+                renderDetail(diaDiem, index)
+              )}
+            </div>
+          )}
         </div>
       </div>
     </>
